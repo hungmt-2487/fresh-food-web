e486675c8bb42cbab5b31ffcdea6e5064b67f861
hungmt-2487 <mai.thanh.hung@sun-asterisk.com>
2022-04-21 09:28:51 +0700

    update db

--- db/migrate/20220419024603_create_oders.rb
+++ db/migrate/20220419024603_create_oders.rb
@@ -6,7 +6,7 @@ class CreateOders < ActiveRecord::Migration[5.1]
       t.string :customer_address, null:false
       t.datetime :shipped_date
       t.integer :status, null:false, index:true
-      t.text :notes
+      t.text :notes, null:true
       t.decimal :total_money, precision: 12, scale: 2, null:false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+# fresh food db
 
 ActiveRecord::Schema.define(version: 20220419025012) do
 
