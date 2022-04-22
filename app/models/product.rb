98cf030dcb11819429b64aa363f8366bdc7abc7c
hungmt-2487 <mai.thanh.hung@sun-asterisk.com>
2022-04-22 16:26:14 +0700

    fix-comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   has_many :order_products
-  has_many :order through: :order_products
-  belongs_to :categories
+  has_many :orders through: :order_products
+  belongs_to :category
 end
