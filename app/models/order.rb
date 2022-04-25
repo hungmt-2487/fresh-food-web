758b0c0cc7c40b92aba03ad8362e46dc1f1d77df
hungmt-2487 <mai.thanh.hung@sun-asterisk.com>
2022-04-25 09:41:21 +0700

    update db, file seed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_products
   has_many :products, through: :order_products
+
+  enum status: [:to_pay, :to_ship, :to_receive, :completed, :cancelled]
 end
